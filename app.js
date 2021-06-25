d4e0dde345f13bcfa1d759edc934b6ccd958019a
Tomer <tomerh1997@gmail.com>
2021-06-25 09:23:36 +0300

    a

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const loginFilter = require('./middleware/login-filter');
 
 
 server.use('/uploads', express.static(__dirname  + '/uploads')); // Make uploads folder public
-server.use(express.static('public')); // Make client public
+server.get('/chef', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+// server.use(express.static('public')); // Make client public
 // server.use(cors({ origin: "http://localhost:4200" })); // For dev only
 server.use(cors());
 server.use(express.json());
@@ -21,9 +24,7 @@ server.use('/api', apiController);
 
 server.use(express.static(path.join(__dirname, 'build')));
 
-server.get('/chef', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 // For PWA support
 // server.all('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, '/public/index.html'));
