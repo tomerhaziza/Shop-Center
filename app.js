d8b4c0db7224dd21a42de58392813cf08b2d47f8
Tomer <tomerh1997@gmail.com>
2021-06-29 15:49:11 +0300

    .

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const server = express();
-const chefServer = express();
 const cors = require('cors');
 const path = require('path')
 const apiController = require('./controllers/api-controller')
@@ -10,13 +9,9 @@ const loginFilter = require('./middleware/login-filter');
 
 server.use('/uploads', express.static(__dirname  + '/uploads')); // Make uploads folder public
 server.use(express.static('public')); // Make client public
-chefServer.use(express.static('build')); // Make chef public
 // server.use(cors({ origin: "http://localhost:4200" })); // For dev only
 server.use(cors());
 server.use(express.json());
-chefServer.use(cors());
-chefServer.use(express.json());
-
 
 // JWT authenticator
 server.use('/api', loginFilter());
@@ -30,11 +25,6 @@ server.all('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 })
 
-chefServer.all('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-})
-
 server.use(errorHandler);
 
 server.listen(process.env.PORT || 3000, () => console.log("Listening on port 3000"));
-chefServer.listen(3001, () => console.log("Chef server listening on port 3001"));
